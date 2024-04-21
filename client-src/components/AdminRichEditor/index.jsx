33862b84b4cbf82b17fd9bbae1467912da2818ef
Crist Gimenez <mail@cri.st>
2024-04-21 20:50:44 -0300

    Default HTML Editor

--- client-src/components/AdminRichEditor/index.jsx
+++ client-src/components/AdminRichEditor/index.jsx
@@ -8,7 +8,7 @@ export default class AdminRichEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'rich',
+      mode: 'html',
 
       isOpenImage: false,
     };
