46eb344f833cfd6fa2d1b605753f3af1d2e8af54
Crist Gimenez <mail@cri.st>
2024-04-21 06:10:28 +0000

    TinyMCE: change initial value to value

--- client-src/components/AdminRichEditor/component/RichEditorQuill/index.jsx
+++ client-src/components/AdminRichEditor/component/RichEditorQuill/index.jsx
@@ -16,7 +16,7 @@ export default class RichEditorQuill extends React.Component {
       <Editor
       licenseKey="gpl"
       apiKey='ngkuqq74ysgvlfrbip87z23lw29m9ejhytniaja6v16z7q77'
-      initialValue={value || ''}
+      value={value || ''}
       init={{
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat | code',
